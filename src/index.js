571e549ce4a33464457c17f183d2783a40d8cbc1
Kamsy <olekakamsy@gmail.com>
2018-12-03 04:07:36 +0100

    all files

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import LoadableApp from "./LoadableApp";
+import * as serviceWorker from "./serviceWorker";
+
+const rootId = document.getElementById("root");
+
+ReactDOM.render(<LoadableApp />, rootId);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+if (module.hot && process.env.NODE_ENV === "development") {
+    module.hot.accept("./LoadableApp", () => {
+        const NextApp = require("./LoadableApp").default;
+        ReactDOM.render(<NextApp />, rootId);
+    });
+}
